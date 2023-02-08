96e9622c02234a17784174153c0639011a11a85b
thanh-bung-5501 <thanhbung5501@gmail.com>
2023-02-08 16:21:43 +0700

    reup new source code

---
+++ SE1611_Group3_A2/Controller/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE1611_Group3_A2.Controller
+{
+    internal class Example
+    {
+        public void Print()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
