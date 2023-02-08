ba8b54321d27a271f65506d3897a04f061e26f58
thanh-bung-5501 <thanhbung5501@gmail.com>
2023-02-08 09:02:39 +0700

    push source code

---
+++ Group3_Assignment2_MusicStoreWPF/Controller/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group3_Assignment2_MusicStoreWPF.Controller
+{
+    internal class Example
+    {
+        public void Print()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
