051dd6309c30a270a16cfe6be7b718b9f25f3cd9
Nguyen Hoai Nam <hoainam151296@gmail.com>
2023-02-15 08:27:24 +0700

    Update fe,be for ShoppingWindow

--- SE1611_Group3_A2/Controller/CartController.cs
+++ SE1611_Group3_A2/Controller/CartController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SE1611_Group3_A2.Controller
 {
@@ -16,8 +17,17 @@ namespace SE1611_Group3_A2.Controller
         }
         public void addCart(Cart cart)
         {
-            context.Carts.Add(cart);
-            context.SaveChanges();
+            try
+            {
+                Cart found = context.Carts.FirstOrDefault(x => x.AlbumId == cart.AlbumId);
+                if (found == null) context.Carts.Add(cart);
+                else found.Count++;
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception");
+            }
         }
 
 
